847ba62cd870839a78ff2429ab20f18e769aed78
rajhansh1994 <38031176+rajhansh1994@users.noreply.github.com>
2021-10-18 19:29:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,20 @@ resource "aws_s3_bucket" "anz_bucket" {
     enabled = var.versioning
   }
 }
+
+data "aws_iam_policy_document" "instance-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "instance" {
+  name               = "instance_role"
+  path               = "/system/"
+  assume_role_policy = data.aws_iam_policy_document.instance-assume-role-policy.json
+}
